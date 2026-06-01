5d051f1c789d68b1deafc5d648bbeab48da4307d
unknown <mvelazquezyarbe@gmail.com>
2026-06-01 03:08:15 +0200

    test: automate TC-14.0 checkout without completing required fields

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,6 +1,6 @@
 describe('Checkout - Sauce Demo', () => {
 
-    it('Completar checkout con datos válidos', () => {
+    beforeEach(() => {
         cy.visit('https://www.saucedemo.com/')
         cy.get('[data-test="username"]').type('standard_user')
         cy.get('[data-test="password"]').type('secret_sauce')
@@ -18,6 +18,9 @@ describe('Checkout - Sauce Demo', () => {
         cy.get('[data-test="cart-contents-container"]').should('be.visible')
         cy.get('[data-test="remove-sauce-labs-backpack"]').should('be.visible')
         cy.get('[data-test="checkout"]').click()
+    })
+
+    it('Completar checkout con datos válidos', () => {
         cy.get('[data-test="firstName"]').type('Juan')
         cy.get('[data-test="lastName"]').type('Pérez')
         cy.get('[data-test="postalCode"]').type('5000')
@@ -26,6 +29,14 @@ describe('Checkout - Sauce Demo', () => {
         cy.get('[data-test="checkout-complete-container"]')
         .should('be.visible')
         .and('contain','Thank you for your order!')
+    })
+
+    it('Completar checkout con datos válidos', () => {
+        cy.get('[data-test="continue"]').click()
+        cy.get('[data-test="error"]')
+        .should('be.visible')
+        .and('contain','Error: First Name is required')
+        cy.get('.summary_info').should('not.exist')
     })
 
 })
